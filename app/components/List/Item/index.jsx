0232012a8780a0e100690b7bf680f07921620a42
weidan123 <weidan@gomeholdings.com>
2017-06-05 18:44:27 +0800

    add(功能)：增加下拉加载

--- app/components/List/Item/index.jsx
+++ app/components/List/Item/index.jsx
@@ -19,14 +19,14 @@ class Item extends Component{
 						</div>
 						<div className="item-content">
 							<div className="item-title-container clear-fix">
-								<h3 className="float-left">{data.title}</h3>
-								<span className="float-right">{data.distance}</span>
+								<h3 className="float-left">合水人家才是真正的</h3>
+								<span className="float-right">100m</span>
 							</div>
 							<p className="item-sub-title">
-								{data.subTitle}
+								很早之前合水人家就是我们向往的美食集合点
 							</p>
 							<div className="item-price-container clear-fix">
-								<span className="price float-left">￥{data.price}</span>
+								<span className="price float-left">￥100</span>
 								<span className="number float-right">已售{data.number}</span>
 							</div>
 						</div>
--- app/containers/Detail/index.jsx
+++ app/containers/Detail/index.jsx
@@ -2,16 +2,23 @@ import React,{Component} from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import HomeList from '../Home/subpage/List'
 import Header from '../../components/Header'
+import LoadMore from '../../components/LoadMore'
 import { getListData } from '../../fetch/home/home'
 import ListData from '../../../mock/home/list'
 import { CITYNAME } from '../../config/localStorekey'
 import LocalStore from '../../util/localStore'
+
+
+import './style.less'
+
+
 class Detail extends Component {
     constructor(props) {
       super(props);
       this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
       this.state = {
         hasMore:true,
+        isLoadingMore:false,
         data:[],
         page:0
       }
@@ -21,8 +28,26 @@ class Detail extends Component {
             <div id="Detail">
                 <Header title="详情"/>
                 <HomeList data={this.state.data}/>
+                <LoadMore isLoadingMore={this.state.isLoadingMore} loadMoreFn={this.loadMoreData.bind(this)}/>
             </div>
         )
+    }
+    loadMoreData(){
+        //记录状态
+        this.setState({
+            isLoadingMore:true
+        })
+        
+
+        const cityName = LocalStore.getItem(CITYNAME)
+        const page = this.state.page
+        const result = getListData(cityName,page)
+        this.resultHandle(result)
+
+        //增加page
+        this.setState({
+            page:page+1
+        })
     }
     componentDidMount(){
         const cityName = LocalStore.getItem(CITYNAME)
