a49048f6b8cd5ea7787511a1da628d25ab8c2858
weidan123 <weidan@gomeholdings.com>
2017-06-06 18:26:29 +0800

    add(增加功能)：增加删除功能

--- app/containers/user/index.jsx
+++ app/containers/user/index.jsx
@@ -1,17 +1,43 @@
 import React,{Component} from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
-class User extends Component {
+const data1 = [
+    {"name":"小红"},{"name":"小名"},{"name":"小明"}
+]
+class Home extends Component{
     constructor(props) {
-      super(props);
-      this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        super(props);
+        this.state = {
+           data:[]
+        }
     }
     render() {
+        let data = this.state.data;
         return (
-            <div id="User">
-                <h5>User</h5>
+            <div>Home
+                {
+                    data.map(function(item,index){
+                        return <div key={index}>Hello, {item.name}!<button onClick={this.clickHandle.bind(this, index)}>删除</button></div>
+                    },this)
+                }
+                <Link to="/city">city</Link>
+                
             </div>
         )
+    }
+     componentDidMount() {
+       this.setState({
+           data:data1
+       })
+        
+    }
+    clickHandle(index) {
+        console.log(index);
+        let data = this.state.data;
+        data.splice(index,1);
+        this.setState({
+            data:data
+        })
     }
 }
 export default User
